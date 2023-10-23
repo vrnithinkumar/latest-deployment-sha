e4501d95e80a8fae80c18b779d16496e0dbeacbe
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2023-10-23 16:02:34 +0000

    Adding input outputs

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,15 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const repo_owner: string = core.getInput('repo_owner')
+    const repo_name: string = core.getInput('repo_name')
+    const env: string = core.getInput('environment')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Finding sha for ${repo_owner}/${repo_name} for env ${env} ...`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('sha', "${repo_owner}/${repo_name}:${env}")
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
