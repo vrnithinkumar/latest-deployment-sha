4d0ce406086a0b2b1d1b96e69bff2a8ac3416e62
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2023-10-23 16:05:11 +0000

    Fix prettier

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ export async function run(): Promise<void> {
     core.debug(`Finding sha for ${repo_owner}/${repo_name} for env ${env} ...`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('sha', "${repo_owner}/${repo_name}:${env}")
+    core.setOutput('sha', '${repo_owner}/${repo_name}:${env}')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
